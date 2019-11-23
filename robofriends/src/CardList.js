9c98f70333013f99e373bc689e9dde1988c0039e
vilagen <vilagen@yahoo.com>
2019-11-22 23:52:22 -0500

    Added a handleChange function.

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -3,14 +3,42 @@ import Card from './Card';
 
 
 const CardList = ( { robots } ) => {
-    const cardArray = robots.map( (user, i) => {
-        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-    })
-    return (
+  return (
     <div>
-      {cardArray}
-    </div>
-    )
-}
+      {
+        robots.map( (users, i) => {
+          return(
+            <Card 
+              key={robots[i].id} 
+              id={robots[i].id} 
+              name={robots[i].name} 
+              email={robots[i].email}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 
 export default CardList;
+
+// another way to write this:
+
+// const CardList = ( { robots } ) => {
+//   const cardArray = robots.map( (user, i) => {
+//       return (
+//          <Card 
+//       key={robots[i].id} 
+//       id={robots[i].id} 
+//       name={robots[i].name} 
+//       email={robots[i].email}
+//       />
+//       );
+//   })
+//   return (
+//   <div>
+//     {cardArray}
+//   </div>
+//   )
+// }
