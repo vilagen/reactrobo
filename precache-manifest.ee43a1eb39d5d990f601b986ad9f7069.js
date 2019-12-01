9765b028fe61f328719f8923498436b240bc4d22
vilagen <vilagen@yahoo.com>
2019-11-30 23:13:11 -0500

    Updates

---
+++ precache-manifest.ee43a1eb39d5d990f601b986ad9f7069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57263269f5fbcc7e0d64b0b377b14b9",
+    "url": "/reactrobo/index.html"
+  },
+  {
+    "revision": "0ada9b46a0134ff2b3d5",
+    "url": "/reactrobo/static/css/main.47d3c139.chunk.css"
+  },
+  {
+    "revision": "2c385a86eedf33318b0e",
+    "url": "/reactrobo/static/js/2.56484c12.chunk.js"
+  },
+  {
+    "revision": "0ada9b46a0134ff2b3d5",
+    "url": "/reactrobo/static/js/main.aeb9ea09.chunk.js"
+  },
+  {
+    "revision": "1b93293bcb515497e492",
+    "url": "/reactrobo/static/js/runtime-main.770d5513.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactrobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
