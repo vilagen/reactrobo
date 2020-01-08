c3ee1521b468149bd26d2da6fcf0e3b5c74aceb4
vilagen <vilagen@yahoo.com>
2020-01-07 23:06:38 -0500

    did some more tests.

--- robofriends/src/reducers.test.js
+++ robofriends/src/reducers.test.js
@@ -9,9 +9,8 @@ import * as reducers from './reducers';
 
 // remember that reducers take two things: a state, and an action.
 describe('searchRobots', () => {
-
   const initialStateSearch = {
-    searchRobots: ''
+    searchField: ''
   };
 
   it('should return the initial state', () => {
@@ -21,11 +20,65 @@ describe('searchRobots', () => {
   it('should handle CHANGE_SEARCH_FIELD', () => {
     expect(reducers.searchRobots(initialStateSearch, {
       type: CHANGE_SEARCH_FIELD,
-      payload: 'abc',
+      payload: 'Clementine'
     })).toEqual({ 
-      searchField: 'abc' 
+      searchField: 'Clementine',
+    });
+  });
+});
+
+describe('requestRobots', () => {
+
+  const initialStateRobots = {
+    robots: [],
+    isPending: false,
+    error: '',
+  };
+
+  it('should return the initial Robot state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+  });
+
+  it('should handle REQUEST_ROBOTS_PENDING action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_PENDING,
+      // payload: { isPending: true } don't need this because reducer is already set to change pending to true.
+    })).toEqual({
+      robots: [],
+      isPending: true,
+      error: '',
+    })
+  })
+
+  it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_SUCCESS,
+      payload: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com',
+      }]
+      // payload: { isPending: true } don't need this because reducer is already set to change pending to true.
+    })).toEqual({
+      robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com',
+      }],
+      isPending: false,
+      error: '',
     });
   });
 
-});
+  it('should handle REQUEST_ROBOTS_FAILED action', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_FAILED,
+      payload: 'NOOOOOOOOOOOOO!!!',
+    })).toEqual({
+       error: 'NOOOOOOOOOOOOO!!!',
+       robots: [],
+       isPending: false,
+    });
+  });
 
+})
